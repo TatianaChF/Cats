58ac136a3844c45e64e399ec317eb49f922f952b
Tatiana Chekina <tikaia.ty@gmail.com>
2022-01-29 22:23:56 +0300

    Adding functionality to add and clear messages

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  message: string[] = [];
+
+  add(message: string) {
+    this.message.push(message);
+  }
+
+  clear() {
+    this.message = [];
+  }
 
   constructor() { }
 }
