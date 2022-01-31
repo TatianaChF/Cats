b59fc16eabc69a06ca7c93df74fad47780fc4e4f
Tatiana Chekina <tikaia.ty@gmail.com>
2022-01-31 18:15:11 +0300

    Added router for breeds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from "./home/home.component";
+import {BreedsComponent} from "./breeds/breeds.component";
+import { BreedDetailComponent } from "./breed-detail/breed-detail.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'detail/:name', component: BreedDetailComponent },
+  { path: 'breeds', component: BreedsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
