69e0614cf98cf3edffb64c85d4b748ef23db504e
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-13 11:37:40 +0300

    Added HttpClient and HttpHeaders, created log() method for displaying messages

--- src/app/breeds.service.ts
+++ src/app/breeds.service.ts
@@ -3,15 +3,20 @@ import { Observable, of } from "rxjs";
 import { BREEDS } from "./more-breeds";
 import { Breeds } from "./breeds";
 import { MessageService } from "./message.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BreedsService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 
-    getBreed(title: string): Observable<Breeds[]> {
+  private log(message: string) {
+    this.messageService.add(`BreedsService: ${message}`);
+  }
+
+  getBreed(title: string): Observable<Breeds[]> {
     const breeds = of(BREEDS);
     this.messageService.add('BreedsService: выбранные породы');
     return breeds;
@@ -19,7 +24,7 @@ export class BreedsService {
 
   getBreeds(title: string): Observable<Breeds> {
     const breed = BREEDS.find(b => b.title === title)!;
-    this.messageService.add(`BreedService: fetched breed title=${title}`);
+    this.messageService.add(`BreedsService: fetched breed title=${title}`);
     return of(breed);
   }
 }
