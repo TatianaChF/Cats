5c89d537b1e964c2981682cb9e23dd921c434c3c
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-13 10:39:31 +0300

    Adding an HTTP service to a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import { BreedsComponent } from './breeds/breeds.component';
+import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import { BreedDetailComponent } from './breed-detail/breed-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BreedsComponent,
+    BreedDetailComponent,
+    MessagesComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule,
+        FormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
