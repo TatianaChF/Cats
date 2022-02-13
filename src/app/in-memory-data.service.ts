551c9389c505e033e3fac706996be685b57f20de
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-13 11:21:53 +0300

    The logic from the more-breeds server has been added to the memory module, now it will act as a server

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Breeds } from "./breeds";
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const breeds = [
+      { id: 1, title: 'Абиссинская кошка' },
+      { id: 2, title: 'Австралийский мист' },
+      { id: 3, title: 'Азиатская табби' },
+      { id: 4, title: 'Акринская' },
+      { id: 5, title: 'Американский бобтейл' },
+      { id: 6, title: 'Американский кёрл' },
+      { id: 7, title: 'Американская короткошерстная' },
+      { id: 8, title: 'Анатолийская' }
+    ];
+    return {breeds};
+  }
+
+  genId(breeds: Breeds[]): number {
+    return breeds.length > 0 ? Math.max (...breeds.map(breed => breed.id)) + 1 : 1;
+  }
 
   constructor() { }
 }
