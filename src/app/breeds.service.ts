0a32f731889fe91554b4960e199c4b80c06c3741
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-14 22:23:15 +0300

    Added error handler

--- src/app/breeds.service.ts
+++ src/app/breeds.service.ts
@@ -4,28 +4,39 @@ import { BREEDS } from "./more-breeds";
 import { Breeds } from "./breeds";
 import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BreedsService {
 
+  private breedsURL = 'api/breeds';
+
   constructor(private messageService: MessageService, private http: HttpClient) { }
 
-  private breedsURL = 'api/breeds';
+  getBreeds(title: string): Observable<Breeds[]> {
+    return this.http.get<Breeds[]>(this.breedsURL)
+      .pipe(
+        catchError(this.handleError<Breeds[]>('getBreeds', []))
+      );
+  }
+
+  getBreed(title: string): Observable<Breeds> {
+    const breed = BREEDS.find(b => b.title === title)!;
+    this.messageService.add(`BreedsService: fetched breed title=${title}`);
+    return of(breed);
+  }
+
   private log(message: string) {
     this.messageService.add(`BreedsService: ${message}`);
   }
 
-  getBreed(title: string): Observable<Breeds[]> {
-    const breeds = of(BREEDS);
-    this.messageService.add('BreedsService: выбранные породы');
-    return breeds;
-  }
-
-  getBreeds(title: string): Observable<Breeds> {
-    const breed = BREEDS.find(b => b.title === title)!;
-    this.messageService.add(`BreedsService: fetched breed title=${title}`);
-    return of(breed);
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      this.log(`${operation} не удалось: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
