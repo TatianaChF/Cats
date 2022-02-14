a790d339c80ef65b65f693861bafb953af4c45fc
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-14 23:19:23 +0300

    Adding a dashboard to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {BreedsComponent} from "./breeds/breeds.component";
 import { BreedDetailComponent } from "./breed-detail/breed-detail.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'detail/:title', component: BreedDetailComponent },
-  { path: 'breeds', component: BreedsComponent }
+  { path: 'breeds/detail/:title', component: BreedDetailComponent },
+  { path: 'breeds', component: BreedsComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
