b0a34ae14a0e491281a282a0add12c30448e78eb
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-15 21:43:56 +0300

    Added breeds search component

---
+++ src/app/breeds-search/breeds-search.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BreedsSearchComponent } from './breeds-search.component';
+
+describe('BreedsSearchComponent', () => {
+  let component: BreedsSearchComponent;
+  let fixture: ComponentFixture<BreedsSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BreedsSearchComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BreedsSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/breeds-search/breeds-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breeds-search',
+  templateUrl: './breeds-search.component.html',
+  styleUrls: ['./breeds-search.component.css']
+})
+export class BreedsSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
