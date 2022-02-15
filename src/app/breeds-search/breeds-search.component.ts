03006f88900c1489949cb519baf37a443367f66c
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-15 22:17:34 +0300

    Added search logic

--- src/app/breeds-search/breeds-search.component.ts
+++ src/app/breeds-search/breeds-search.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from "rxjs";
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from "rxjs/operators";
+
+import { Breeds } from "../breeds";
+import { BreedsService } from "../breeds.service";
 
 @Component({
   selector: 'app-breeds-search',
@@ -6,10 +13,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./breeds-search.component.css']
 })
 export class BreedsSearchComponent implements OnInit {
+  breeds$!: Observable<Breeds[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private breedsService: BreedsService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.breeds$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term:string) => this.breedsService.searchBreeds(term)),
+    );
   }
 
 }
