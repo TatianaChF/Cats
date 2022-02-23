e6e1acc99b6d1c3567a5dc6e428ac15a2788a991
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-23 22:46:31 +0300

    Add component not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import {HomeComponent} from "./home/home.component";
 import {BreedsComponent} from "./breeds/breeds.component";
 import { BreedDetailComponent } from "./breed-detail/breed-detail.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'breeds/detail/:title', component: BreedDetailComponent },
   { path: 'breeds', component: BreedsComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'detail/:id', component: BreedDetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
