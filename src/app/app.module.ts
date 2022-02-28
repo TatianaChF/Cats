4ef2c7d409f26281d0c12671bc6a1f22badf4bdd
Tatiana Chekina <tikaia.ty@gmail.com>
2022-02-28 17:50:42 +0300

    Memory module import fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
-import { InMemoryDataService } from "../../../../Angular/angular-tour-of-heroes/src/app/in-memory-data.service";
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import { BreedsComponent } from './breeds/breeds.component';
-import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import { BreedDetailComponent } from './breed-detail/breed-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BreedsSearchComponent } from './breeds-search/breeds-search.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -27,16 +27,18 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BreedsSearchComponent,
     NotFoundComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        RouterModule,
-        FormsModule,
-        HttpClientModule,
-        HttpClientInMemoryWebApiModule.forRoot(
-          InMemoryDataService, { dataEncapsulation: false }
-        )
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    ),
+    CommonModule,
+    CommonModule,
+    CommonModule
+  ],
   providers: [],
   exports: [BreedsSearchComponent],
   bootstrap: [AppComponent]
