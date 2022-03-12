60ff0bff917429d5d5a5e58b411246a74250b5c3
Tatiana Chekina <tikaia.ty@gmail.com>
2022-03-12 11:48:23 +0300

    Added function to display rocks on click

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 import { Breed } from "../breeds";
 import { BreedsService } from "../breeds.service";
@@ -10,10 +11,16 @@ import { BreedsService } from "../breeds.service";
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
+  breeds: Breed[] = [];
 
-  constructor() { }
+  constructor(private breedsService: BreedsService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getVarias();
   }
 
+  getVarias(): void {
+    this.breedsService.getBreeds()
+      .subscribe(breeds => this.breeds = breeds);
+  }
 }
