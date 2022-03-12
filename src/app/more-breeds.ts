2b06eacbe84ddf50506f0b346500c111c870ff1c
Tatiana Chekina <tikaia.ty@gmail.com>
2022-03-12 17:12:34 +0300

    Changing the filter category

--- src/app/more-breeds.ts
+++ src/app/more-breeds.ts
@@ -1,12 +1,12 @@
 import { Breed } from "./breeds";
 
 export const BREEDS: Breed[] = [
-  { id: 1, title: 'Абиссинская кошка', image: 'assets/images/breeds/Abyssinian_cat.svg', alt: 'Photo Abyssinian Cat', species: 'Short Haired' },
-  { id: 2, title: 'Австралийский мист', image: 'assets/images/breeds/mist.svg', alt: 'Photo Australian Mist', species: 'Short Haired'  },
-  { id: 3, title: 'Азиатская табби', image: 'assets/images/breeds/Asian_tabby.svg', alt: 'Photo Asian Tabby', species: 'Short Haired'  },
-  { id: 4, title: 'Акринская', image: 'assets/images/breeds/Akrinskaya.svg', alt: 'Photo Akrinskaya', species: 'Short Haired'  },
-  { id: 5, title: 'Американский бобтейл', image: 'assets/images/breeds/american_bob.svg', alt: 'Photo American Bobtail', species: 'Varia'  },
-  { id: 6, title: 'Американский кёрл', image: 'assets/images/breeds/american_curl.svg', alt: 'Photo American Curl', species: 'Varia'  },
-  { id: 7, title: 'Американская короткошерстная', image: 'assets/images/breeds/american_shorthair.svg', alt: 'Photo American Shorthair', species: 'Short Haired'  },
-  { id: 8, title: 'Анатолийская', image: 'assets/images/breeds/Anatolian.svg', alt: 'Photo Anatolian', species: 'Short Haired'  }
+  { id: 1, title: 'Абиссинская кошка', image: 'assets/images/breeds/Abyssinian_cat.svg', alt: 'Photo Abyssinian Cat', filter: 'Короткошерстные' },
+  { id: 2, title: 'Австралийский мист', image: 'assets/images/breeds/mist.svg', alt: 'Photo Australian Mist', filter: 'Короткошерстные'  },
+  { id: 3, title: 'Азиатская табби', image: 'assets/images/breeds/Asian_tabby.svg', alt: 'Photo Asian Tabby', filter: 'Короткошерстные'  },
+  { id: 4, title: 'Акринская', image: 'assets/images/breeds/Akrinskaya.svg', alt: 'Photo Akrinskaya', filter: 'Короткошерстные'  },
+  { id: 5, title: 'Американский бобтейл', image: 'assets/images/breeds/american_bob.svg', alt: 'Photo American Bobtail', filter: 'Все'  },
+  { id: 6, title: 'Американский кёрл', image: 'assets/images/breeds/american_curl.svg', alt: 'Photo American Curl', filter: 'Все'  },
+  { id: 7, title: 'Американская короткошерстная', image: 'assets/images/breeds/american_shorthair.svg', alt: 'Photo American Shorthair', filter: 'Короткошерстные'  },
+  { id: 8, title: 'Анатолийская', image: 'assets/images/breeds/Anatolian.svg', alt: 'Photo Anatolian', filter: 'Короткошерстные'  }
 ];
