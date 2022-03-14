1ddec4da109a420d97707b3d5e479de8743dc1e9
Tatiana Chekina <tikaia.ty@gmail.com>
2022-03-14 21:30:33 +0300

    Create a list of categories

---
+++ src/app/all-filters.ts
@@ -0,0 +1,8 @@
+import { Filter } from "./filter";
+
+export const FILTER: Filter[] = [
+  { id: 1, title: 'Все породы' },
+  { id: 2, title: 'Длинношерстные' },
+  { id: 3, title: 'Короткошерстные' },
+  { id: 4, title: 'Лысые' }
+];
