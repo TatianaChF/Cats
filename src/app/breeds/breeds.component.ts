67532a476c778890f68315471ede8e105e4d3216
Tatiana Chekina <tikaia.ty@gmail.com>
2022-03-22 15:42:49 +0300

    Filter function created

--- src/app/breeds/breeds.component.ts
+++ src/app/breeds/breeds.component.ts
@@ -12,16 +12,39 @@ export class BreedsComponent implements OnInit {
 
   breeds: Breed[] = [];
   searchKey: string = "";
+  public filterCategory: any;
 
   constructor(private breedsService: BreedsService) { }
 
   ngOnInit(): void {
     this.getBreeds();
+    this.breedsService.search.subscribe((val:any) => {
+      this.searchKey = val;
+    })
   }
 
   getBreeds(): void {
     this.breedsService.getBreeds()
-        .subscribe(breeds => this.breeds = breeds);
+      .subscribe(breeds => {
+        this.breeds = breeds;
+        this.filterCategory = breeds;
+
+        this.breeds.forEach((breed: any) => {
+            if(breed.category === "Все") {
+              breed.category = "Все";
+            }
+          }
+        )});
+    console.log(this.breeds);
+  }
+
+  filter(category: string) {
+    this.filterCategory = this.breeds
+      .filter((breed: any) => {
+        if(breed.category == category || category == "") {
+          return breed;
+        }
+      })
   }
 
 }
