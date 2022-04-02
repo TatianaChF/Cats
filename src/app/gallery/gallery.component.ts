c3f97c419d6a00f8a7fc74416cfa3eda88249638
Tatiana Chekina <tikaia.ty@gmail.com>
2022-04-02 09:58:55 +0300

    Importing an array of photos

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GALLERY } from "../more-photo";
 
 @Component({
   selector: 'app-gallery',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+
+  gallery = GALLERY;
 
   constructor() { }
 
