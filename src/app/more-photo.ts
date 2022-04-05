5b40b33c122ad3aaf4c51c2ff76004f1580fd741
Tatiana Chekina <tikaia.ty@gmail.com>
2022-04-05 17:38:36 +0300

    Changed order of images

--- src/app/more-photo.ts
+++ src/app/more-photo.ts
@@ -1,19 +1,9 @@
 import { Gallery } from "./gallery";
 
 export const GALLERY: Gallery[] = [
-  { id: 1, image: 'assets/images/gallery/images_1.jpg', alt: 'Photo one' },
-  { id: 2, image: 'assets/images/gallery/images_2.jpg', alt: 'Photo two' },
-  { id: 3, image: 'assets/images/gallery/images_3.jpg', alt: 'Photo three' },
-  { id: 4, image: 'assets/images/gallery/images_4.jpg', alt: 'Photo four' },
-  { id: 5, image: 'assets/images/gallery/images_5.jpg', alt: 'Photo five' },
-  { id: 6, image: 'assets/images/gallery/images_6.jpg', alt: 'Photo six' },
-  { id: 7, image: 'assets/images/gallery/images_7.jpg', alt: 'Photo seven' },
-  { id: 8, image: 'assets/images/gallery/images_8.jpg', alt: 'Photo eight' },
-  { id: 9, image: 'assets/images/gallery/images_9.jpg', alt: 'Photo nine' },
-  { id: 10, image: 'assets/images/gallery/images_10.jpg', alt: 'Photo ten' },
-  { id: 11, image: 'assets/images/gallery/images_11.jpg', alt: 'Photo eleven' },
-  { id: 12, image: 'assets/images/gallery/images_12.jpg', alt: 'Photo twelve' },
-  { id: 13, image: 'assets/images/gallery/images_13.jpg', alt: 'Photo thirteen' },
-  { id: 14, image: 'assets/images/gallery/images_14.jpg', alt: 'Photo fourteen' },
-  { id: 15, image: 'assets/images/gallery/images_15.jpg', alt: 'Photo fifteen' }
+  { id: 1, image: 'assets/images/gallery/images_1.jpg', alt: 'Photo one', styles: 'gallery_item_1' },
+  { id: 2, image: 'assets/images/gallery/images_4.jpg', alt: 'Photo two', styles: 'gallery_item_4' },
+  { id: 3, image: 'assets/images/gallery/images_3.jpg', alt: 'Photo three', styles: 'gallery_item_3' },
+  { id: 4, image: 'assets/images/gallery/images_2.jpg', alt: 'Photo four', styles: 'gallery_item_2' },
+  { id: 5, image: 'assets/images/gallery/images_5.jpg', alt: 'Photo five', styles: 'gallery_item_5' }
 ]
