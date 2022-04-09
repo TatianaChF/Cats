5b5c7a828e1edbc353d26e695a0b8a9733274ad8
Tatiana Chekina <tikaia.ty@gmail.com>
2022-04-09 11:11:42 +0300

    New photos added

--- src/app/more-photo.ts
+++ src/app/more-photo.ts
@@ -5,5 +5,10 @@ export const GALLERY: Gallery[] = [
   { id: 2, image: 'assets/images/gallery/images_4.jpg', alt: 'Photo two', styles: 'gallery_item_4' },
   { id: 3, image: 'assets/images/gallery/images_3.jpg', alt: 'Photo three', styles: 'gallery_item_3' },
   { id: 4, image: 'assets/images/gallery/images_2.jpg', alt: 'Photo four', styles: 'gallery_item_2' },
-  { id: 5, image: 'assets/images/gallery/images_5.jpg', alt: 'Photo five', styles: 'gallery_item_5' }
-]
+  { id: 5, image: 'assets/images/gallery/images_5.jpg', alt: 'Photo five', styles: 'gallery_item_5' },
+  { id: 6, image: 'assets/images/gallery/images_6.jpg', alt: 'Photo six', styles: 'gallery_item_6' },
+  { id: 7, image: 'assets/images/gallery/images_7.jpg', alt: 'Photo seven', styles: 'gallery_item_7' },
+  { id: 8, image: 'assets/images/gallery/images_8.jpg', alt: 'Photo eight', styles: 'gallery_item_8' },
+  { id: 9, image: 'assets/images/gallery/images_9.jpg', alt: 'Photo nine', styles: 'gallery_item_9' },
+  { id: 10, image: 'assets/images/gallery/images_10.jpg', alt: 'Photo ten', styles: 'gallery_item_10' }
+];
