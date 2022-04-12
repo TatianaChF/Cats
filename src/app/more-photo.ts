5e80da5207aa1bd1f66f8908b37cbd60a9fa2a12
Tatiana Chekina <tikaia.ty@gmail.com>
2022-04-12 22:49:50 +0300

    Changing images

--- src/app/more-photo.ts
+++ src/app/more-photo.ts
@@ -1,14 +1,19 @@
 import { Gallery } from "./gallery";
 
 export const GALLERY: Gallery[] = [
-  { id: 1, image: 'assets/images/gallery/images_1.jpg', alt: 'Photo one', styles: 'gallery_item_1' },
-  { id: 2, image: 'assets/images/gallery/images_4.jpg', alt: 'Photo two', styles: 'gallery_item_4' },
-  { id: 3, image: 'assets/images/gallery/images_3.jpg', alt: 'Photo three', styles: 'gallery_item_3' },
-  { id: 4, image: 'assets/images/gallery/images_2.jpg', alt: 'Photo four', styles: 'gallery_item_2' },
-  { id: 5, image: 'assets/images/gallery/images_5.jpg', alt: 'Photo five', styles: 'gallery_item_5' },
-  { id: 6, image: 'assets/images/gallery/images_6.jpg', alt: 'Photo six', styles: 'gallery_item_6' },
-  { id: 7, image: 'assets/images/gallery/images_7.jpg', alt: 'Photo seven', styles: 'gallery_item_7' },
-  { id: 8, image: 'assets/images/gallery/images_8.jpg', alt: 'Photo eight', styles: 'gallery_item_8' },
-  { id: 9, image: 'assets/images/gallery/images_9.jpg', alt: 'Photo nine', styles: 'gallery_item_9' },
-  { id: 10, image: 'assets/images/gallery/images_10.jpg', alt: 'Photo ten', styles: 'gallery_item_10' }
+  { id: 1, image: 'assets/images/gallery/1.jpg', alt: 'Photo one', styles: 'gallery_item_1', row: '1', col: '1' },
+  { id: 2, image: 'assets/images/gallery/2.jpg', alt: 'Photo two', styles: 'gallery_item_4', row: '1', col: '2' },
+  { id: 3, image: 'assets/images/gallery/3.jpg', alt: 'Photo three', styles: 'gallery_item_3', row: '1', col: '3' },
+  { id: 4, image: 'assets/images/gallery/4.jpg', alt: 'Photo four', styles: 'gallery_item_2', row: '2', col: '1' },
+  { id: 5, image: 'assets/images/gallery/5.jpg', alt: 'Photo five', styles: 'gallery_item_5', row: '2', col: '2' },
+  { id: 6, image: 'assets/images/gallery/6.jpg', alt: 'Photo six', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 7, image: 'assets/images/gallery/7.jpg', alt: 'Photo seven', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 8, image: 'assets/images/gallery/8.jpg', alt: 'Photo eight', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 9, image: 'assets/images/gallery/9.jpg', alt: 'Photo nine', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 10, image: 'assets/images/gallery/10.jpg', alt: 'Photo ten', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 11, image: 'assets/images/gallery/11.jpg', alt: 'Photo eleven', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 12, image: 'assets/images/gallery/12.jpg', alt: 'Photo twelve', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 13, image: 'assets/images/gallery/13.jpg', alt: 'Photo thirteen', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 14, image: 'assets/images/gallery/14.jpg', alt: 'Photo fourteen', styles: 'gallery_item_6', row: '2', col: '3' },
+  { id: 15, image: 'assets/images/gallery/15.jpg', alt: 'Photo fifteen', styles: 'gallery_item_6', row: '2', col: '3' }
 ];
