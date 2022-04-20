9d622075a248fd60d06a23edb109cb5b9593ae25
Tatiana Chekina <tikaia.ty@gmail.com>
2022-04-20 21:52:28 +0300

    Creating a Gallery Carousel Component

---
+++ src/app/gallery-carousel/gallery-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-carousel',
+  templateUrl: './gallery-carousel.component.html',
+  styleUrls: ['./gallery-carousel.component.css']
+})
+export class GalleryCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
