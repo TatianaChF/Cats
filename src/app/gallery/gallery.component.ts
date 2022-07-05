ba2dbf70e76fb11ae686ec3f9f37bcc66797334c
Tatiana Chekina <tikaia.ty@gmail.com>
2022-07-05 17:51:39 +0300

    Adding Selected Image Variable and Image Array

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { GALLERY } from "../more-photo";
+import {Component, OnInit} from '@angular/core';
+import {Gallery} from "../gallery";
 
 @Component({
   selector: 'app-gallery',
@@ -8,7 +8,25 @@ import { GALLERY } from "../more-photo";
 })
 export class GalleryComponent implements OnInit {
 
-  gallery = GALLERY;
+  public selectedImage?: number | null = null;
+
+  public images = [
+    { id: 1, image: 'assets/images/gallery/1.jpg', alt: 'Photo one', styles: 'gallery_item_1', row: '1', col: '1' },
+    { id: 2, image: 'assets/images/gallery/2.jpg', alt: 'Photo two', styles: 'gallery_item_4', row: '1', col: '2' },
+    { id: 3, image: 'assets/images/gallery/3.jpg', alt: 'Photo three', styles: 'gallery_item_3', row: '1', col: '3' },
+    { id: 4, image: 'assets/images/gallery/4.jpg', alt: 'Photo four', styles: 'gallery_item_2', row: '2', col: '1' },
+    { id: 5, image: 'assets/images/gallery/5.jpg', alt: 'Photo five', styles: 'gallery_item_5', row: '2', col: '2' },
+    { id: 6, image: 'assets/images/gallery/6.jpg', alt: 'Photo six', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 7, image: 'assets/images/gallery/7.jpg', alt: 'Photo seven', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 8, image: 'assets/images/gallery/8.jpg', alt: 'Photo eight', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 9, image: 'assets/images/gallery/9.jpg', alt: 'Photo nine', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 10, image: 'assets/images/gallery/10.jpg', alt: 'Photo ten', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 11, image: 'assets/images/gallery/11.jpg', alt: 'Photo eleven', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 12, image: 'assets/images/gallery/12.jpg', alt: 'Photo twelve', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 13, image: 'assets/images/gallery/13.jpg', alt: 'Photo thirteen', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 14, image: 'assets/images/gallery/14.jpg', alt: 'Photo fourteen', styles: 'gallery_item_6', row: '2', col: '3' },
+    { id: 15, image: 'assets/images/gallery/15.jpg', alt: 'Photo fifteen', styles: 'gallery_item_6', row: '2', col: '3' }
+  ] as Array<Gallery>;
 
   constructor() { }
 
