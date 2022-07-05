6f189f922493a167ebdb69e484c0d78788744fbc
Tatiana Chekina <tikaia.ty@gmail.com>
2022-07-05 17:48:51 +0300

    Importing animations, creating a close function and image swiping functions, an event handler function

--- src/app/gallery-carousel/gallery-carousel.component.ts
+++ src/app/gallery-carousel/gallery-carousel.component.ts
@@ -1,17 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-import { GALLERY } from "../more-photo";
-import { GalleriaModule } from 'primeng/galleria';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Gallery} from "../gallery";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-gallery-carousel',
   templateUrl: './gallery-carousel.component.html',
-  styleUrls: ['./gallery-carousel.component.css']
+  styleUrls: ['./gallery-carousel.component.css'],
+  animations: [
+    trigger('slide-in', [
+      state('left', style({
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      })),
+      state('middle', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+        })),
+      state('right', style({
+        opacity: 0,
+        transform: 'translateX(100%)'
+      })),
+      transition('left => middle', [
+        style({
+          opacity: 0,
+          transform: 'translateX(100%)'
+        }),
+        animate('250ms')
+      ]),
+      transition('middle => *', [
+        animate('250ms')
+      ]),
+      transition('right => middle', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100%)'
+        }),
+        animate('250ms')
+      ])
+    ])
+  ]
 })
 export class GalleryCarouselComponent implements OnInit {
 
-  gallery = GALLERY;
+  @Input() images: Array<Gallery> = [];
+  @Input() selectedImage!: number;
+  @Output() closeCarousel = new EventEmitter<void>();
+  public animState = 'middle';
+  public animImage!: number;
+
+  constructor() { }
 
   ngOnInit(): void {
+    this.animImage = this.selectedImage;
+  }
+
+  public close(): void {
+    this.closeCarousel.emit();
+  }
+
+  public previous(): void {
+    this.animState = 'left';
+    this.selectedImage = this.selectedImage > 0 ? this.selectedImage - 1 : this.images.length - 1;
+    this.blur();
+  }
+
+  public next(): void {
+    this.animState = 'right';
+    this.selectedImage = this.selectedImage < this.images.length - 1 ? this.selectedImage + 1 : 0;
+    this.blur();
+  }
+
+  public animDone(): void {
+    this.animImage = this.selectedImage;
+    this.animState = 'middle';
+  }
+
+  public onEvent(event: Event): void {
+    event.stopPropagation();
+  }
+
+  private blur(): void {
+    const activeElement = document.activeElement as HTMLElement;
+    if(activeElement !== null) {
+      activeElement.blur();
+    }
   }
 
 }
