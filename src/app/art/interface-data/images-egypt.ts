f132f8fded6dda01273fe83123b06ffa435a788b
Tatiana Chekina <tikaia.ty@gmail.com>
2022-07-13 21:34:29 +0300

    Creating a file for storing images for text about Egypt

---
+++ src/app/art/interface-data/images-egypt.ts
@@ -0,0 +1,9 @@
+import {Gallery} from "../../interfaces/gallery";
+
+export const IMAGE: Gallery[] = [
+  { id: 1, image: 'assets/images/art/Ra and Apop.jpg', alt: 'Ra and Apop', styles: 'images_one' },
+  { id: 2, image: 'assets/images/art/cat.jpg', alt: 'Cat', styles: 'images_two' },
+  { id: 3, image: 'assets/images/art/cat on the mosaic.webp', alt: 'Cat On The Mosaic', styles: 'images_three' },
+  { id: 4, image: 'assets/images/art/Bastet.jpg', alt: 'Bastet', styles: 'images_four' },
+  { id: 5, image: 'assets/images/art/lactating cat.jpg', alt: 'Lactating Cat', styles: 'images_five' }
+];
