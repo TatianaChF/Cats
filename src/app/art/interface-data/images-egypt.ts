f6d81528d583631a1334b5e416cb445236091bb5
Tatiana Chekina <tikaia.ty@gmail.com>
2022-07-14 10:59:52 +0300

    Adding images for a paragraph about different views on cats

--- src/app/art/interface-data/images-egypt.ts
+++ src/app/art/interface-data/images-egypt.ts
@@ -1,9 +1,30 @@
 import {Gallery} from "../../interfaces/gallery";
 
-export const IMAGE: Gallery[] = [
-  { id: 1, image: 'assets/images/art/Ra and Apop.jpg', alt: 'Ra and Apop', styles: 'images_one' },
-  { id: 2, image: 'assets/images/art/cat.jpg', alt: 'Cat', styles: 'images_two' },
-  { id: 3, image: 'assets/images/art/cat on the mosaic.webp', alt: 'Cat On The Mosaic', styles: 'images_three' },
-  { id: 4, image: 'assets/images/art/Bastet.jpg', alt: 'Bastet', styles: 'images_four' },
-  { id: 5, image: 'assets/images/art/lactating cat.jpg', alt: 'Lactating Cat', styles: 'images_five' }
+export const IMAGE_EGYPT: Gallery[] = [
+  { id: 1, image: 'assets/images/art/Ancient Egypt/Ra and Apop.jpg', alt: 'Ra and Apop', styles: 'images_one' },
+  { id: 2, image: 'assets/images/art/Ancient Egypt/cat.jpg', alt: 'Cat', styles: 'images_two' },
+  { id: 3, image: 'assets/images/art/Ancient Egypt/cat on the mosaic.webp', alt: 'Cat On The Mosaic', styles: 'images_three' },
+  { id: 4, image: 'assets/images/art/Ancient Egypt/Bastet.jpg', alt: 'Bastet', styles: 'images_four' },
+  { id: 5, image: 'assets/images/art/Ancient Egypt/lactating cat.jpg', alt: 'Lactating Cat', styles: 'images_five' }
 ];
+
+export const IMAGE_DIFFERENT: Gallery[] = [
+  { id: 6, image: 'assets/images/art/different view/Last Supper.jpg', alt: 'Last Supper', styles: 'images_six' },
+  { id: 7, image: 'assets/images/art/different view/The Visit to the Spinner.webp', alt: 'The Visit to the Spinner', styles: 'images_seven' },
+  { id: 8, image: 'assets/images/art/different view/The Garden of Earthly Delights.webp', alt: 'The Garden of Earthly Delights', styles: 'images_eight' },
+  { id: 9, image: 'assets/images/art/different view/Boy.webp', alt: 'Boy', styles: 'images_nine' },
+  { id: 10, image: 'assets/images/art/different view/Birth of John the Baptist.webp', alt: 'Birth of John the Baptist', styles: 'images_ten' },
+  { id: 11, image: 'assets/images/art/different view/Cat fight in pantry.jpg', alt: 'Cat fight in pantry', styles: 'images_eleven' },
+  { id: 12, image: 'assets/images/art/different view/Alexander Adriansen.jpg', alt: 'Alexander Adriansen', styles: 'images_twelve' },
+  { id: 13, image: 'assets/images/art/different view/Annibale Caracci.jpg', alt: 'Annibale Caracci', styles: 'images_thirteen' },
+  { id: 14, image: 'assets/images/art/different view/Bartolomeo Orbotori.jpg', alt: 'Bartolomeo Orbotori', styles: 'images_fourteen' },
+  { id: 15, image: 'assets/images/art/different view/Van Bockle.jpg', alt: 'Van Bockle', styles: 'images_fifteen' },
+  { id: 16, image: 'assets/images/art/different view/David de Conning.jpg', alt: 'David de Conning', styles: 'images_sixteen' },
+  { id: 17, image: 'assets/images/art/different view/Bagelier.jpg', alt: 'Bagelier', styles: 'images_seventeen' },
+  { id: 18, image: 'assets/images/art/different view/The Godolphin Arabian.webp', alt: 'The Godolphin Arabian', styles: 'images_eighteen' },
+  { id: 19, image: 'assets/images/art/different view/Cats Suggested As The Fifty-three Stations of the Tōkaidō.webp', alt: 'Cats Suggested As The Fifty-three Stations of the Tōkaidō',
+    styles: 'images_nineteen' },
+  { id: 20, image: 'assets/images/art/different view/hissing cat.jpg', alt: 'Hissing Cat', styles: 'twenty' },
+  { id: 21, image: 'assets/images/art/different view/royal cat.jpg', alt: 'Royal Cat', styles: 'twenty_one' },
+  { id: 22, image: 'assets/images/art/different view/cat and flowers.jpg', alt: 'Cat And Flowers', styles: 'twenty_two' }
+]
