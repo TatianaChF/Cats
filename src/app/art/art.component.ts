696db24b03ae50f3aa68fde7aabccfe5be0f50ec
Tatiana Chekina <tikaia.ty@gmail.com>
2022-07-19 15:20:45 +0300

    Adding variables to display images

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {IMAGE_AFTER, IMAGE_DIFFERENT, IMAGE_EGYPT} from "./interface-data/images-egypt";
 
 @Component({
   selector: 'app-art',
@@ -6,6 +7,12 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./art.component.css']
 })
 export class ArtComponent implements OnInit {
+
+  public images_egypt = IMAGE_EGYPT;
+  public images_different = IMAGE_DIFFERENT;
+  public images_after = IMAGE_AFTER;
+  public animState = 'middle';
+  public animImage!: number;
 
   constructor() { }
 
